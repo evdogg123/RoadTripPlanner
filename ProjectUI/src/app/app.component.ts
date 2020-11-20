bf596dfebe85b0f1d67b42012c47b72e767b64d3
Sam Zabaldo <szabaldo@udel.edu>
2020-11-20 14:15:45 -0500

    fixed redirect on sign-out, styled login

--- ProjectUI/src/app/app.component.ts
+++ ProjectUI/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthService } from './services/auth.service';
+import { TripsService } from './trips.service';
 
 @Component({
   selector: 'app-root',
@@ -11,13 +12,15 @@ export class AppComponent {
   get loggedIn():boolean{
     return this.authSvc.loggedIn;
   }
-  constructor(public authSvc:AuthService) {
+  constructor(public authSvc:AuthService, public tripSvc:TripsService) {
     console.log("trying to authorize....")
     authSvc.authorize();
   }
 
   signout(){
     this.authSvc.logout();
+    this.tripSvc.trips.length = 0; // this is a bit of workaround to make it seem 
+                                   // like the page is refreshing on sign-out
     return false;
   }
 }
