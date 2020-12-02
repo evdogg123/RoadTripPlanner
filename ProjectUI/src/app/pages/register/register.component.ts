d786886005c7466c5685d7de1b6a33ffd2bba047
Benjamin Testani <testanib@udel.edu>
2020-12-02 14:23:31 -0500

    reverting to stephens code for fixing registering

--- ProjectUI/src/app/pages/register/register.component.ts
+++ ProjectUI/src/app/pages/register/register.component.ts
@@ -38,7 +38,7 @@ export class RegisterComponent implements OnInit {
       return;
     }
     this.loading=true;
-    this.authSvc.register(this.signupForm.controls.username.value,this.signupForm.controls.password.value).subscribe(response=>{
+    /* this.authSvc.register(this.signupForm.controls.username.value,this.signupForm.controls.password.value).subscribe(response=>{
       console.log(response);
       this.router.navigate([this.returnUrl]);
     },err=>{
@@ -55,7 +55,16 @@ export class RegisterComponent implements OnInit {
       this.alertService.success('Account Created.  Redirecting to login...', this.options);
       //this.error=err.message||err;
 
-    });
+    }); */
+    this.authSvc.register(this.signupForm.controls.username.value,this.signupForm.controls.password.value).subscribe(response=>{
+      this.router.navigate([this.returnUrl]);
+      console.log("Account Creation successful, logging in");
+
+      this.authSvc.login(this.signupForm.controls.username.value,this.signupForm.controls.password.value).subscribe(response=>{
+        this.router.navigate([this.returnUrl]);
+      },err=>{this.submitted=false;this.loading=false;this.error=err.message||err;});
+
+    },err=>{this.submitted=false;this.loading=false;this.error=err.message||err;});
   }
 
 }
