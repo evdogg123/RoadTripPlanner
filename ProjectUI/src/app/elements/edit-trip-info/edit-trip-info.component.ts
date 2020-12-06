ea5e9f4032e268bd2be80e8ec5d66c03b10418c2
MarioDurso <mariod@udel.edu>
2020-12-06 17:32:27 -0500

    made the update card refresh the home page

--- ProjectUI/src/app/elements/edit-trip-info/edit-trip-info.component.ts
+++ ProjectUI/src/app/elements/edit-trip-info/edit-trip-info.component.ts
@@ -3,6 +3,8 @@ import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms'
 import { ActivatedRoute, Router } from '@angular/router';
 import { EditTripInfoService } from 'src/app/services/edit-trip-info.service';
 import { ProjectsService } from 'src/app/services/projects.service';
+import { AlertService } from '../../elements/_alert';
+import { TripsService } from 'src/app/trips.service';
 
 @Component({
   selector: 'app-edit-trip-info',
@@ -18,11 +20,15 @@ export class EditTripInfoComponent implements OnInit {
   error: string;
   tripId: string;
   state;
+  options = {
+    autoClose: true,
+    keepAfterRouteChange: false}
   
 
 
 
-  constructor(private formBuilder: FormBuilder, private route: ActivatedRoute, private router: Router, private projSvc:ProjectsService, public edittripSvc: EditTripInfoService) { }
+  constructor(private formBuilder: FormBuilder, protected alertService: AlertService, private route: ActivatedRoute,
+     private router: Router, private projSvc:ProjectsService, public edittripSvc: EditTripInfoService, public tripsSvc: TripsService,) { }
 
   ngOnInit(): void {
     this.tripId = ''; 
@@ -40,8 +46,18 @@ editTrip() {
   console.log(this.tripForm.value.name);
   this.projSvc.editTrip({name:this.tripForm.value.name, description:this.tripForm.value.description}, this.edittripSvc.tripId).subscribe(result =>{
     console.log(result);
-    this.edittripSvc.show_box = false;
-    this.router.navigateByUrl("/home")
+    this.alertService.success('Trip Updates.  Redirecting to home...', this.options);
+    setTimeout(() =>{
+      this.router.navigateByUrl("/home");
+      this.projSvc.getTrips().subscribe(result=>{
+        console.log(result.data);
+        this.tripsSvc.trips=result.data;
+      });
+      this.edittripSvc.show_box = false; 
+      this.updating = false; 
+      this.submitted = false; 
+      this.tripForm.reset(); 
+    }, 2000);
     
   })
 }
