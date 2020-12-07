2f74330618af1357097bd48ee0c49eebe162027c
evdogg123 <evanmac@udel.edu>
2020-12-07 18:47:55 -0500

    whoooole lotta big fixes/ styling stuff

--- ProjectUI/src/app/app.module.ts
+++ ProjectUI/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, } from '@angular/core';
-import { AgmCoreModule} from '@agm/core';
+import { AgmCoreModule } from '@agm/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
@@ -13,31 +13,31 @@ import { TokenInterceptorService } from './services/token-interceptor.service';
 import { TripComponent } from './pages/trip/trip.component';
 import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
-import { CalendarEvent,CalendarEventAction,CalendarEventTimesChangedEvent,CalendarView,} from 'angular-calendar';
+import { CalendarEvent, CalendarEventAction, CalendarEventTimesChangedEvent, CalendarView, } from 'angular-calendar';
 import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
-import {startOfDay,endOfDay,subDays,addDays,endOfMonth,isSameDay,isSameMonth,addHours,} from 'date-fns';
+import { startOfDay, endOfDay, subDays, addDays, endOfMonth, isSameDay, isSameMonth, addHours, } from 'date-fns';
 import { CalendarComponent } from './pages/calendar/calendar.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FlatpickrModule } from 'angularx-flatpickr';
-import {MatSidenavModule,} from '@angular/material/sidenav';
+import { MatSidenavModule, } from '@angular/material/sidenav';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import {ChangeDetectorRef} from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { ChangeDetectorRef } from '@angular/core';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { SubtripComponent } from './pages/subtrip/subtrip.component';
 import { TripCardComponent } from './elements/trip-card/trip-card.component';
 import { AlertModule } from './elements/_alert';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatButtonToggleModule} from '@angular/material/button-toggle';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatSelectModule} from '@angular/material/select';
-import {MatNativeDateModule} from '@angular/material/core';
-import {MatInputModule} from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatInputModule } from '@angular/material/input';
 import { AddTripComponent } from './elements/add-trip/add-trip.component';
 import { EditTripInfoComponent } from './elements/edit-trip-info/edit-trip-info.component';
-
+import { NgxSpinnerModule } from "ngx-spinner";
 @NgModule({
   declarations: [
     AppComponent,
@@ -60,6 +60,7 @@ import { EditTripInfoComponent } from './elements/edit-trip-info/edit-trip-info.
     MatSidenavModule,
     MatButtonModule,
     MatIconModule,
+    NgxSpinnerModule,
     BrowserModule,
     HttpClientModule,
     BrowserAnimationsModule,
@@ -79,10 +80,10 @@ import { EditTripInfoComponent } from './elements/edit-trip-info/edit-trip-info.
     CalendarModule.forRoot({
       provide: DateAdapter,
       useFactory: adapterFactory,
-      
+
     }),
     AlertModule
-   
+
 
   ],
   providers: [{ provide: HTTP_INTERCEPTORS, useClass: TokenInterceptorService, multi: true }],
--- ProjectUI/src/app/services/projects.service.ts
+++ ProjectUI/src/app/services/projects.service.ts
@@ -7,44 +7,44 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ProjectsService {
 
-  private path="http://localhost:3000/api/trips/"
-  constructor(private http:HttpClient) { }
+  private path = "http://localhost:3000/api/trips/"
+  constructor(private http: HttpClient) { }
 
-  getTrips(): Observable<any>{
+  getTrips(): Observable<any> {
     return this.http.get(this.path);
   }
-  addTrips(data: any):Observable<any>{
+  addTrips(data: any): Observable<any> {
     return this.http.post<any>(this.path, data);//.subscribe(res => console.log('success', res));
   }
-  addSubTrip(data: any, tripId: string){
+  addSubTrip(data: any, tripId: string) {
     return this.http.post<any>(`${this.path}trip/${tripId}`, data).subscribe(res => console.log('success', res));
   }
-  getTrip(tripId: string): Observable<any>{
-    return this.http.get( `${this.path}trip/${tripId}`);
+  getTrip(tripId: string): Observable<any> {
+    return this.http.get(`${this.path}trip/${tripId}`);
   }
-  deleteTrip(data:any, tripId: string){
+  deleteTrip(data: any, tripId: string) {
     return this.http.delete(`${this.path}trip/${tripId}`, data);
   }
-  deleteSubTrip(data: any, tripId: string){
-    return this.http.put(`${this.path}trip/${tripId}`, data).subscribe(res => console.log('success', res));
+  deleteSubTrip(data: any, tripId: string) {
+    return this.http.put(`${this.path}trip/${tripId}`, data);
   }
-  editTrip(data: any, tripId: string){
+  editTrip(data: any, tripId: string) {
     return this.http.put(`${this.path}trip/${tripId}/edit`, data)//.subscribe(res => console.log('success', res));
   }
-  editSubTrip(data: any, tripId: string){
+  editSubTrip(data: any, tripId: string) {
     return this.http.put(`${this.path}trip/${tripId}/editSubTrip`, data)//.subscribe(res => console.log('success', res));
   }
-  updateCenter(data:any, tripID:string){
+  updateCenter(data: any, tripID: string) {
     return this.http.post<any>(`${this.path}trip/${tripID}/center`, data).subscribe(res => console.log('success', res));
   }
-  getLocationSummary(data:string){
+  getLocationSummary(data: string) {
     return this.http.get(`${this.path}wikiSearch/${data}`);
   }
-  addActivity(data:any, tripId:string, subTripId: string){
+  addActivity(data: any, tripId: string, subTripId: string) {
     return this.http.put(`${this.path}trip/${tripId}/${subTripId}`, data);
   }
-  getSubTrip(tripId:string, subTripId: string){
+  getSubTrip(tripId: string, subTripId: string) {
     return this.http.get(`${this.path}trip/${tripId}/${subTripId}`);
   }
-  }
+}
 
